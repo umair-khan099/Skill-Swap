169e520c5937a9c6cbd1bf47d060e2f9dfa9e94a
Umair khan <ukkhan5476@gmail.com>
2026-06-04 11:29:58 +0530

    feat: restructure server codebase, migrate to TypeScript, and add otp-generator dependency

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import { CONFIG } from "./config/dotenv.config.js";
+import { app } from "./app.js";
+import { dbConnect } from "./db/dbConnect.js";
+
+ (async () => {
+  await dbConnect();
+  app.listen(CONFIG.PORT, () => {
+    console.log(`Server is Runing at PORT :: ${CONFIG.PORT}`);
+  });
+})();
