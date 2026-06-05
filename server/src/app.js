b212959c5d8b3aa1df8323409a05251fdf65b923
Umair khan <ukkhan5476@gmail.com>
2026-06-05 16:11:34 +0530

    refactor: remove TypeScript files and migrate server codebase to JavaScript

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
-import { CONFIG } from "./config/dotenv.config.js";
-import { errorMiddleware } from "./middleware/error.middleware.js";
-import { authRouter } from "./routes/auth.route.js";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import indexRoute from "./routes/index.route";
 
 // app
 export const app = express();
@@ -17,10 +17,17 @@ app.use(
   }),
 );
 
+// attacks prevent global Middlewares
+
+app.use(helmet());
+app.use(hpp());
+app.use(compression());
+
 // middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/api/v1/auth", authRouter);
+
+app.use("/api/v1",indexRoute);
 
 app.use(errorMiddleware);
