b3cbc63142bd1381e6c4ef5f259b7191172b6cce
Umair khan <ukkhan5476@gmail.com>
2026-06-05 17:53:10 +0530

    feat: add dotenv configuration and database connection, implement error handling utilities

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cors from "cors";
 import hpp from "hpp";
 import compression from "compression";
-import indexRoute from "./routes/index.route";
+import indexRoute from "./routes/index.route.js";
+import { CONFIG } from "./config/dotenv.config.js";
 
 // app
 export const app = express();
@@ -27,7 +29,6 @@ app.use(compression());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/api/v1", indexRoute);
 
-app.use("/api/v1",indexRoute);
-
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
